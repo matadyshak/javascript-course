eb163e02f9515025b4346151892bb94e867b2394
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-08-17 22:37:54 -0500

    Fixed 12t to change Auto Play button text

--- 3-my-exercise-solutions/lesson-12/scripts/12t.js
+++ 3-my-exercise-solutions/lesson-12/scripts/12t.js
@@ -24,19 +24,18 @@ let intervalId;
 //};
 function autoPlay() {
   if (!isAutoPlaying) {
-    
+    document.querySelector('.js-auto-play-button').innerHTML = 'Stop Playing';    
     intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
     isAutoPlaying = true;
-    document.querySelector('.js-auto-play-button').innerHTML = 'Auto-Play';
     
 
   } else {
+    document.querySelector('.js-auto-play-button').innerHTML = 'Auto-Play';
     clearInterval(intervalId);
     isAutoPlaying = false;
-    document.querySelector('.js-auto-play-button').innerHTML = 'Stop Playing';
   }
 }
 
