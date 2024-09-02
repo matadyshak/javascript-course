8c9e4b7c198053120d567a18f42ddbd9f1f2bad4
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-09-01 21:25:17 -0500

    import of calculateCartQuantity requires ../amazon.js / took back out script module cart.js

--- 3-my-exercise-solutions/lesson-14/scripts/checkout.js
+++ 3-my-exercise-solutions/lesson-14/scripts/checkout.js
@@ -1,7 +1,7 @@
 import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
-import {calculateCartQuantity} from './amazon.js';
+import {calculateCartQuantity} from '../amazon.js';
 
 let cartSummaryHTML = '';
 
@@ -17,7 +17,6 @@ cart.forEach((cartItem) => {
   })
 
   cartSummaryHTML +=
-  
   `
   <div class="cart-item-container 
     js-cart-item-container-${matchingProduct.id}">
