c2ecb3be141064653806c60a59932d00d4f7cc69
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-09-15 12:35:58 -0500

    Exercise 15h - Do not call container delete when deleting a project.  Call renderOrderSummary()

--- 3-my-exercise-solutions/lesson-15/scripts/checkout/orderSummary.js
+++ 3-my-exercise-solutions/lesson-15/scripts/checkout/orderSummary.js
@@ -81,10 +81,10 @@ document.querySelector('.js-order-summary')
       const productId = link.dataset.productIdDelete;
       removeFromCart(productId);
       
-      const container = document.querySelector(
-        `.js-cart-item-container-${productId}`
-      );
-      container.remove();
+//      const container = document.querySelector(
+//        `.js-cart-item-container-${productId}`
+//      );
+//      container.remove();
       renderOrderSummary();
       renderPaymentSummary();
     }); // addEventListener
