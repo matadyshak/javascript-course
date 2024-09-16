28b9976d38f077d2a9e685a25a1d96bc13f46e52
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-09-15 20:11:56 -0500

    Removed Jasmine default tests and made tests for formatCurrency() using Jasmine.

--- 3-my-exercise-solutions/lesson-16/scripts/utils/money.js
+++ 3-my-exercise-solutions/lesson-16/scripts/utils/money.js
@@ -1,6 +1,7 @@
 export function formatCurrency(priceCents) {
   //Get around rounding issue that occurs if Math.round() is not used here
   return (Math.round(priceCents) / 100).toFixed(2);
+//  return (priceCents / 100).toFixed(2);
 }
 
 //Default export - does not need {}
--- 3-my-exercise-solutions/lesson-16/tests/moneyTest.js
+++ 3-my-exercise-solutions/lesson-16/tests/moneyTest.js
@@ -9,7 +9,7 @@ if (formatCurrency(2095) === '20.95') {
   console.log('failed');
 }
 
-console.log('Works with zero');
+console.log('Works with 0');
 if (formatCurrency(0) === '0.00') {
   console.log('passed');
 } else {
