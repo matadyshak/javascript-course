2049cdbc7075774b65e9b7be85c6a1e1be42eb8c
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-09-25 22:44:22 -0500

    Just getting started with end-to-end megatest

---
+++ 3-my-exercise-solutions/lesson-16/tests/checkout/paymentSummaryTest.js
@@ -0,0 +1,28 @@
+import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
+import {loadFromStorage, cart, initCartForTest} from '../../data/cart.js';
+
+describe('test suite: End-to-End Mega-Test', () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // describe
+
+
+
+
+
+
