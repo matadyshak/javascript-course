17e3eb8a0408cb9b6d189b3576e4ab84f67e2ac5
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-09-26 08:11:04 -0500

    Still working on mega-test exercise 16N

--- 3-my-exercise-solutions/lesson-16/tests/checkout/paymentSummaryTest.js
+++ 3-my-exercise-solutions/lesson-16/tests/checkout/paymentSummaryTest.js
@@ -1,8 +1,70 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
-import {loadFromStorage, cart, initCartForTest} from '../../data/cart.js';
+import {cart, initCartForTest} from '../../data/cart.js';
 
 describe('test suite: End-to-End Mega-Test', () => {
 
+  const productId1 = '10ed8504-57db-433c-b0a3-fc71a35c88a1'; //Pink sneakers $33.90
+  const productId2 = 'id1'; // Men's smokin hot backpack $19.99
+  const productId3 = 'id2'; // Men's umbrella $49.99
+
+  beforeEach( () => {
+    spyOn(localStorage, 'setItem');
+
+  document.querySelector('.js-test-container').innerHTML = `
+    <div class="checkout-header js-checkout-header"></div>
+    <div class="order-summary js-order-summary"></div>
+    <div class="payment-summary js-payment-summary"></div>
+    <div class="product-quantity-container">
+      <select class="js-quantity-selector-${productId1}">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option selected value="10">10</option>
+      </select>
+    </div>
+    <div class="product-quantity-container">
+      <select class="js-quantity-selector-${productId2}">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option selected value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+    <div class="product-quantity-container">
+      <select class="js-quantity-selector-${productId3}">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option selected value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+    <div class="added-to-cart js-added-to-cart-${productId1}">
+    </div>
+    <div class="added-to-cart js-added-to-cart-${productId2}">
+    </div>
+    <div class="added-to-cart js-added-to-cart-${productId3}">
+    </div>
+
+    `;
+  }); // beforeEach
 
 
 
