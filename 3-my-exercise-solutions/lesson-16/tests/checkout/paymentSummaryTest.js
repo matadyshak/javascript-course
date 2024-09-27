ed9052f50c115c41d5fc120abf89134ec9030999
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-09-26 22:37:43 -0500

    Added delivery options testing to mega-test.  Not passing yet.  Exercise 16N.

--- 3-my-exercise-solutions/lesson-16/tests/checkout/paymentSummaryTest.js
+++ 3-my-exercise-solutions/lesson-16/tests/checkout/paymentSummaryTest.js
@@ -1,17 +1,19 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
+import {renderPaymentSummary} from '../../scripts/checkout/paymentSummary.js';
+import {renderCheckoutHeader} from '../../scripts/checkout/checkoutHeader.js';
 import {cart, addToCart, initCartForTest} from '../../data/cart.js';
 
 describe('test suite: End-to-End Mega-Test', () => {
 
   const productId1 = '10ed8504-57db-433c-b0a3-fc71a35c88a1'; //Pink sneakers $33.90
-  const productId2 = 'id1'; // Men's smokin hot backpack $19.99
+  const productId2 = 'id1'; // Men's smokin' hot backpack $19.99
   const productId3 = 'id2'; // Men's umbrella $49.99
 
   beforeEach( () => {
     spyOn(localStorage, 'setItem');
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
-    });
+  });
 
   document.querySelector('.js-test-container').innerHTML = `
     <div class="checkout-header js-checkout-header"></div>
@@ -65,8 +67,12 @@ describe('test suite: End-to-End Mega-Test', () => {
     </div>
     <div class="added-to-cart js-added-to-cart-${productId3}">
     </div>
-
     `;
+
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+
   }); // beforeEach
 
   afterEach( () => {
@@ -79,13 +85,22 @@ describe('test suite: End-to-End Mega-Test', () => {
     let cartItems = [];
     initCartForTest(cartItems);  
 
-    //should add a qty of 10 from HTML above
-    addToCart(productId1);
-    expect(cart.length).toEqual(1);
+    addToCart(productId1); // qty 10
+    addToCart(productId2); // qty 5
+    addToCart(productId3); // qty 3 
+    addToCart(productId3); // qty 6
+
+    expect(cart.length).toEqual(3);
     expect(cart[0].productId).toEqual(productId1);
     expect(cart[0].quantity).toEqual(10);
     expect(cart[0].deliveryOptionId).toEqual('1');
-    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[1].productId).toEqual(productId2);
+    expect(cart[1].quantity).toEqual(5);
+    expect(cart[1].deliveryOptionId).toEqual('1');
+    expect(cart[2].productId).toEqual(productId3);
+    expect(cart[2].quantity).toEqual(6);
+    expect(cart[2].deliveryOptionId).toEqual('1');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(4);
     expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify(
       [
         { productId: productId1,
@@ -95,7 +110,72 @@ describe('test suite: End-to-End Mega-Test', () => {
       ]
     )); //CalledWith stringify
 
-    expect(document.querySelector('.js-cart-quantity').value).toEqual(10);
+    expect(document.querySelector(`.js-product-name-${productId1}`).innerText)
+      .toContain('Waterproof Knit Athletic Sneakers - Pink');
+    expect(document.querySelector(`.js-product-price-${productId1}`).innerText).toEqual('$33.90');
+
+    expect(document.querySelector(`.js-product-name-${productId2}`).innerText)
+      .toContain('Men\'s Smokin\' Hot Backpack');
+    expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual('$19.99');
+
+    expect(document.querySelector(`.js-product-name-${productId3}`).innerText)
+      .toContain('Men\'s Black Full-Size Umbrella');
+    expect(document.querySelector(`.js-product-price-${productId3}`).innerText).toEqual('$49.99');
+
+    expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toEqual('10');
+    expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toEqual('5');
+    expect(document.querySelector(`.js-product-quantity-${productId3}`).innerText).toEqual('6');
+
+    expect(document.querySelector('.js-checkout-header').value).toContain('21');  //
+    expect(document.querySelector('.js-cart-quantity').value).toContain('21');
+    expect(document.querySelector('.js-total-cart-quantity').value).toContain('21');
+
+    expect(document.querySelector('.js-payment-summary-price'   ).innerText).toEqual('$738.89');
+    expect(document.querySelector('.js-payment-summary-shipping').innerText).toEqual('$0.00');
+    expect(document.querySelector('.js-payment-summary-subtotal').innerText).toEqual('$738.89');
+    expect(document.querySelector('.js-payment-summary-tax'     ).innerText).toEqual('$73.89');
+    expect(document.querySelector('.js-payment-summary-total'   ).innerText).toEqual('$812.78');
+
+      // Change delivery options - These will run the rendering code
+      document.querySelector(`.js-delivery-option-input-${productId1}-3`).click();
+      document.querySelector(`.js-delivery-option-input-${productId2}-2`).click();
+      document.querySelector(`.js-delivery-option-input-${productId3}-3`).click();
+
+      expect(document.querySelector(`.js-delivery-option-input-${productId1}-1`).checked).toEqual(false);
+      expect(document.querySelector(`.js-delivery-option-input-${productId1}-2`).checked).toEqual(false);
+      expect(document.querySelector(`.js-delivery-option-input-${productId1}-3`).checked).toEqual(true);
+      expect(document.querySelector(`.js-delivery-option-input-${productId2}-1`).checked).toEqual(false);
+      expect(document.querySelector(`.js-delivery-option-input-${productId2}-2`).checked).toEqual(true);
+      expect(document.querySelector(`.js-delivery-option-input-${productId2}-3`).checked).toEqual(false);
+      expect(document.querySelector(`.js-delivery-option-input-${productId3}-1`).checked).toEqual(true);
+      expect(document.querySelector(`.js-delivery-option-input-${productId3}-2`).checked).toEqual(false);
+      expect(document.querySelector(`.js-delivery-option-input-${productId3}-3`).checked).toEqual(false);
+      
+      expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(3);
+      expect(document.querySelector(`.js-cart-item-container-${productId1}`)).not.toEqual(null);
+      expect(document.querySelector(`.js-cart-item-container-${productId2}`)).not.toEqual(null);
+      expect(document.querySelector(`.js-cart-item-container-${productId3}`)).not.toEqual(null);
+      
+      expect(cart[0].productId).toEqual(productId1);
+      expect(cart[0].quantity).toEqual(10);
+      expect(cart[0].deliveryOptionId).toEqual('3');
+      expect(cart[1].productId).toEqual(productId2);
+      expect(cart[1].quantity).toEqual(5);
+      expect(cart[1].deliveryOptionId).toEqual('2');
+      expect(cart[2].productId).toEqual(productId3);
+      expect(cart[2].quantity).toEqual(6);
+      expect(cart[2].deliveryOptionId).toEqual('3');
+            
+      expect(document.querySelector('.js-payment-summary-price'   ).innerText).toEqual('$738.89');
+      expect(document.querySelector('.js-payment-summary-shipping').innerText).toEqual('$24.97');
+      expect(document.querySelector('.js-payment-summary-subtotal').innerText).toEqual('$763.86');
+      expect(document.querySelector('.js-payment-summary-tax'     ).innerText).toEqual('$76.39');
+      expect(document.querySelector('.js-payment-summary-total'   ).innerText).toEqual('$840.25');
+  
+    // Change quantities using Update, Text, Save
+    // Test scheduled delivery dates 
+    // Delete all products
+
     }); // it()
 }); // describe
 
