4d66a65e3e7bace765f0f08a9dff5fdb6822e257
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-10-11 15:48:14 -0500

    Make localStorageKey and loadFromStorage() private

--- 3-my-exercise-solutions/lesson-17/data/cart-class.js
+++ 3-my-exercise-solutions/lesson-17/data/cart-class.js
@@ -1,15 +1,15 @@
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey;
   
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
   
   
-    loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)); 
+    #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)); 
 
   // Note: [] an empty array is a truthy value which makes !cart falsey so the below 
   // code does not run when testing
@@ -27,7 +27,7 @@ class Cart {
 }
 
 saveToStorage() {
-  localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
 }
 
 addToCart(productId) {
