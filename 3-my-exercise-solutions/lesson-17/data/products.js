211a7a68f05378910c1031af6aab3e2d7fb92fcd
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-10-11 18:34:06 -0500

    Removed temporary code that showed an example of initializing a Product class instance

--- 3-my-exercise-solutions/lesson-17/data/products.js
+++ 3-my-exercise-solutions/lesson-17/data/products.js
@@ -26,24 +26,6 @@ class Product {
   }
 }
 
-const product1 = new Product({
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  });
-
-console.log(product1);
-
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -722,4 +704,4 @@ export const products = [
 ].map((productDetails) => {
       return new Product(productDetails);
 });
-console.log(Products);
+
