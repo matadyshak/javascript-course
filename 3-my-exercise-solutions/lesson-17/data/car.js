b3371e6938488961f4ad5b7bfb4a14f86baff780
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-10-13 14:05:10 -0500

    Working Exercise 17d - OpenTrunk() CloseTrunk()

--- 3-my-exercise-solutions/lesson-17/data/car.js
+++ 3-my-exercise-solutions/lesson-17/data/car.js
@@ -2,18 +2,25 @@ class Car {
   brand;
   model;
   speed;
+  isTrunkOpen;
 
   constructor(carinfo){
     this.brand = carinfo.brand;
     this.model = carinfo.model;
     this.speed = 0;
+    this.isTrunkOpen = false;
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+    const trunkStatus = (this.isTrunkOpen) ? 'Trunk: Open' : 'Trunk: Closed';
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h ${trunkStatus}`);
   }
 
   go () {
+    if(this.isTrunkOpen) {
+      return;
+    }
+    
     if (this.speed <= 195) {
       this.speed += 5;
     }
@@ -26,6 +33,15 @@ class Car {
       this.speed = 0;
     }
   }
+
+openTrunk()  {
+  if (this.speed === 0) {
+    this.isTrunkOpen = true;
+  }
+}
+
+closeTrunk()  {
+  this.isTrunkOpen = false;
 }
 
 const car1 = new Car({brand: 'Toyota', model: 'Corolla'});
@@ -38,6 +54,17 @@ car1.displayInfo();
 car2.displayInfo();
 
 for( let i=0; i<42; i++) {
+  if( i === 0) {
+    car1.openTrunk();
+    car1.displayInfo();
+    car1.closeTrunk();
+    car1.displayInfo();
+ } else if ( i === 10) {
+    car1.openTrunk();
+    car1.displayInfo();
+    car1.closeTrunk();
+    car1.displayInfo();
+ } else {
   car1.go();
   car1.displayInfo();
   car2.go();
