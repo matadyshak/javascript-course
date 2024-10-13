f0b8e63f30e38ac37b6484395e9fee3b12050a72
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-10-13 10:08:23 -0500

    Exercise 17c - add speed parameter and go() and brake() methods

--- 3-my-exercise-solutions/lesson-17/data/car.js
+++ 3-my-exercise-solutions/lesson-17/data/car.js
@@ -1,14 +1,28 @@
 class Car {
   brand;
   model;
+  speed;
 
   constructor(carinfo){
     this.brand = carinfo.brand;
     this.model = carinfo.model;
+    this.speed = 0;
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this,model}`);
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed}`);
+  }
+
+  go () {
+    if (this.speed <= 195) {
+      this.speed += 5;
+    }
+  }
+
+  brake () {
+    if (this.speed >= 5) {
+      this.speed -= 5;
+    }
   }
 }
 
@@ -21,3 +35,15 @@ console.log(car2);
 car1.displayInfo();
 car2.displayInfo();
 
+for( let i=0; i<40; i++) {
+  car1.go();
+  car1.displayInfo();
+  car2.go();
+  car2.displayInfo();
+}
+for(let j=0; j<40; j++) {
+  car1.brake();
+  car1.displayInfo();
+  car2.brake();
+  car2.displayInfo();
+}
