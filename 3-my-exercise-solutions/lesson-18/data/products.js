8ec179eb27fe6a4d45a0078655db05ec8391cecc
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-10-19 22:18:47 -0500

    Load products from backend server supersimpledev.  Problem that I changed 'id' to 'productId'

--- 3-my-exercise-solutions/lesson-18/data/products.js
+++ 3-my-exercise-solutions/lesson-18/data/products.js
@@ -126,6 +126,29 @@ const object4 = {
 object4.method();
 */
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'appliance') {
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/product');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -819,3 +842,5 @@ export const products = [
   }
   return new Product(productDetails);
 });
+
+*/
