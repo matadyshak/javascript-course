4e1c918510429035d36e1e9b00c2d42f91cd0e6a
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-10-19 22:20:18 -0500

    Other changes

--- 3-my-exercise-solutions/lesson-18/scripts/amazon.js
+++ 3-my-exercise-solutions/lesson-18/scripts/amazon.js
@@ -1,105 +1,110 @@
 import {cart} from '../data/cart-class.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js'
 
-function displayCartQuantity()
-{ 
-  // Reads and displays cart total quantity on the go to cart page button
-  // Does not update the cart or localStorage
-  const cartQuantity = cart.calculateCartQuantity();
-  document.querySelector('.js-cart-quantity-amazon').innerHTML = cartQuantity;
-  return;
-}
+loadProducts(renderProductsGrid);
 
-let productsHTML = '';
+function renderProductsGrid() {
 
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-    <div class="product-image-container">
-      <img class="product-image"
-        src="${product.image}">
-    </div>
+  let productsHTML = '';
 
-    <div class="product-name limit-text-to-2-lines">
-      ${product.name}
-    </div>
-
-    <div class="product-rating-container">
-      <img class="product-rating-stars"
-        src="${product.getStarsUrl()}">
-      <div class="product-rating-count link-primary">
-        ${product.rating.count}
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
       </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="${product.getStarsUrl()}">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        ${product.getPrice()}
+      </div>
+
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.productId}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+      
+      ${product.extraInfoHTML()}
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-added-to-cart-${product.productId}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.productId}">
+        Add to Cart
+      </button>
     </div>
+    `;
 
-    <div class="product-price">
-      ${product.getPrice()}
-    </div>
+  });  //forEach((product
 
-    <div class="product-quantity-container">
-      <select class="js-quantity-selector-${product.productId}">
-        <option selected value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-      </select>
-    </div>
-    
-    ${product.extraInfoHTML()}
+  //Display all products in the web page
+  //This will return null if we are not on the main page
+  const element = document.querySelector('.js-products-grid');
+  element.innerHTML = productsHTML;
 
-    <div class="product-spacer"></div>
+  displayCartQuantity();
 
-    <div class="added-to-cart js-added-to-cart-${product.productId}">
-      <img src="images/icons/checkmark.png">
-      Added
-    </div>
+  //Get selectors to all 42 Add to Cart buttons
+  document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
 
-    <button class="add-to-cart-button button-primary js-add-to-cart"
-    data-product-id="${product.productId}">
-      Add to Cart
-    </button>
-  </div>
-  `;
+      //Add a click event listener for each of the 42 buttons
+      button.addEventListener('click', () => {
 
-});  //forEach((product
+      //For each button get the unique product id
+      //dataset contains data- attributes
+      //attribute name product-id becomes camelCase productId in the dataset object
+      //The code below will run for any individual button that gets clicked
+      //const productId = button.dataset.productId;
+      //Destructuring shortcut
+      const {dataset} = button;
+      const {productId} = dataset;
 
-//Display all products in the web page
-//This will return null if we are not on the main page
-const element = document.querySelector('.js-products-grid');
-element.innerHTML = productsHTML;
+      cart.addToCart(productId);
+      displayCartQuantity();
 
-displayCartQuantity();
+      }) // button.addEventListener
+    }) //forEach(button)
+  }
 
-//Get selectors to all 42 Add to Cart buttons
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-
-    //Add a click event listener for each of the 42 buttons
-    button.addEventListener('click', () => {
-
-    //For each button get the unique product id
-    //dataset contains data- attributes
-    //attribute name product-id becomes camelCase productId in the dataset object
-    //The code below will run for any individual button that gets clicked
-    //const productId = button.dataset.productId;
-    //Destructuring shortcut
-    const {dataset} = button;
-    const {productId} = dataset;
-
-    cart.addToCart(productId);
-    displayCartQuantity();
-
-    }) // button.addEventListener
-  }) //forEach(button)
-
-////////////////////////////////////////////////////////////////////////////////
+  function displayCartQuantity()
+  { 
+    // Reads and displays cart total quantity on the go to cart page button
+    // Does not update the cart or localStorage
+    const cartQuantity = cart.calculateCartQuantity();
+    document.querySelector('.js-cart-quantity-amazon').innerHTML = cartQuantity;
+    return;
+  }
+  
+  ////////////////////////////////////////////////////////////////////////////////
 // Get a variable out of a file
 // 1. Add type="module" attribute
 // 2. Export
