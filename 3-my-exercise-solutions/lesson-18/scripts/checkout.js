d5a5c6d35cff92f9e567694dba34771dc18a268c
matadyshak <161066263+matadyshak@users.noreply.github.com>
2024-10-25 14:49:52 -0500

    Changes for using a Promise

--- 3-my-exercise-solutions/lesson-18/scripts/checkout.js
+++ 3-my-exercise-solutions/lesson-18/scripts/checkout.js
@@ -2,26 +2,60 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 //import '../data/car.js';
 
+Promise.all([
+  new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+}),
 
 new Promise((resolve) => {
-  console.log('start promise');
-  loadProducts(() => {
-    console.log('finished loading');
+  cart.loadCart(() => {
     resolve();
-  });
-}).then(() => {
-  console.log('next step');
-})
-
-
-/*
-loadProducts(() => {
+    });
+  })
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
+});
+
+/*
+//Top-level code
+//Promises keep our code more flat instead of nested
+//Use promises instead of callbacks
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+    cart.loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+});
+*/
+
+/*
+//Multiple callbacks causes complex nested code that is hard to work with
+loadProducts(() => {
+  cart.loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+  });
 });
 */
